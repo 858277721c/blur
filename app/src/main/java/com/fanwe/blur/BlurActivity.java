100d81fe38f7c185ae3558d9c4759b586c97ea18
zjun <565061763@qq.com>
2018-07-06 12:04:29 +0800

    demo

--- app/src/main/java/com/fanwe/blur/BlurActivity.java
+++ app/src/main/java/com/fanwe/blur/BlurActivity.java
@@ -2,7 +2,6 @@ package com.fanwe.blur;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -14,17 +13,13 @@ import java.util.Random;
 
 public class BlurActivity extends AppCompatActivity
 {
-    public static final int COLOR_OVERLAY_TRANSPARENT = Color.TRANSPARENT;
-    public static final int COLOR_OVERLAY_RED = Color.parseColor("#66FF0000");
     private final TimeLogger mTimeLogger = new TimeLogger(BlurActivity.class.getSimpleName());
-
     private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        mTimeLogger.setDebug(true);
         setContentView(R.layout.act_blur);
         mImageView = findViewById(R.id.imageview);
 
@@ -45,12 +40,5 @@ public class BlurActivity extends AppCompatActivity
                 mTimeLogger.print("blurry");
             }
         });
-    }
-
-
-    @Override
-    protected void onDestroy()
-    {
-        super.onDestroy();
     }
 }
--- app/src/main/java/com/fanwe/blur/TimeLogger.java
+++ app/src/main/java/com/fanwe/blur/TimeLogger.java
@@ -7,32 +7,22 @@ public class TimeLogger
     private final String mLogTag;
     private long mStartTime;
 
-    private boolean mIsDebug;
-
     public TimeLogger(String logTag)
     {
         mLogTag = logTag;
     }
 
-    public void setDebug(boolean debug)
-    {
-        mIsDebug = debug;
-    }
 
     public void start()
     {
-        if (mIsDebug)
-            mStartTime = System.currentTimeMillis();
+        mStartTime = System.currentTimeMillis();
     }
 
     public void print(String preffix)
     {
-        if (mIsDebug)
-        {
-            if (preffix == null)
-                preffix = "time";
+        if (preffix == null)
+            preffix = "time";
 
-            Log.i(mLogTag, preffix + ":" + (System.currentTimeMillis() - mStartTime));
-        }
+        Log.i(mLogTag, preffix + ":" + (System.currentTimeMillis() - mStartTime));
     }
 }

754a23b22fe639dd2237a9da63ab86e7b8e6ff99
zjun <565061763@qq.com>
2018-07-07 21:42:46 +0800

    BlurConfig

--- lib/src/main/java/com/fanwe/lib/blur/core/Blur.java
+++ lib/src/main/java/com/fanwe/lib/blur/core/Blur.java
@@ -3,43 +3,8 @@ package com.fanwe.lib.blur.core;
 import android.graphics.Bitmap;
 import android.view.View;
 
-public interface Blur
+public interface Blur extends BlurConfig
 {
-    /**
-     * 设置模糊半径，默认10
-     *
-     * @param radius
-     */
-    void setRadius(int radius);
-
-    /**
-     * 设置压缩倍数，默认8
-     *
-     * @param downSampling
-     */
-    void setDownSampling(int downSampling);
-
-    /**
-     * 设置覆盖层颜色，默认透明
-     *
-     * @param colorOverlay
-     */
-    void setColorOverlay(int colorOverlay);
-
-    /**
-     * 设置返回的模糊Bitmap是否要保持压缩后的宽和高，默认false-保持原来的宽和高
-     *
-     * @param keepDownSamplingSize true-保持压缩后的宽和高，false-保持原来的宽和高
-     */
-    void setKeepDownSamplingSize(boolean keepDownSamplingSize);
-
-    /**
-     * 返回压缩倍数
-     *
-     * @return
-     */
-    int getDownSampling();
-
     /**
      * 得到View的模糊Bitmap
      *

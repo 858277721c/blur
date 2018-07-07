70d268822d8bed37b79fe985fc2860d76504921c
zjun <565061763@qq.com>
2018-07-07 18:13:37 +0800

    refactor

--- lib/src/main/java/com/fanwe/lib/blur/api/BlurApi.java
+++ lib/src/main/java/com/fanwe/lib/blur/api/BlurApi.java
@@ -16,7 +16,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-public abstract class BlurApi<S, R>
+abstract class BlurApi<S, R>
 {
     private static final ExecutorService EXECUTOR_SERVICE = Executors.newCachedThreadPool();
 

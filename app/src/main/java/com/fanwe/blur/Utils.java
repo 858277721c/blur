9b05b89417564035d33fb710fceb535e14e30a97
zjun <565061763@qq.com>
2018-07-08 18:48:33 +0800

    demo

--- app/src/main/java/com/fanwe/blur/Utils.java
+++ app/src/main/java/com/fanwe/blur/Utils.java
@@ -8,10 +8,21 @@ import java.util.Random;
 
 public class Utils
 {
+    private static int sIndex = -1;
+
     public static Bitmap randomBitmap(Context context)
     {
-        final int index = new Random().nextInt(10);
-        final int resId = context.getResources().getIdentifier("fj" + index, "drawable", context.getPackageName());
+        while (true)
+        {
+            final int index = new Random().nextInt(10);
+            if (index == sIndex)
+                continue;
+
+            sIndex = index;
+            break;
+        }
+
+        final int resId = context.getResources().getIdentifier("fj" + sIndex, "drawable", context.getPackageName());
         final Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), resId);
         return bitmap;
     }

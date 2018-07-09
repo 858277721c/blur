16f7fbf2225d568215d7b22126d6c9d760114902
zjun <565061763@qq.com>
2018-07-09 22:57:37 +0800

    refactor

--- lib/src/main/java/com/fanwe/lib/blur/view/BlurViewAttrs.java
+++ lib/src/main/java/com/fanwe/lib/blur/view/BlurViewAttrs.java
@@ -2,7 +2,6 @@ package com.fanwe.lib.blur.view;
 
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.graphics.Color;
 import android.util.AttributeSet;
 
 import com.fanwe.lib.blur.DefaultBlurSettings;
@@ -10,29 +9,34 @@ import com.fanwe.lib.blur.R;
 
 class BlurViewAttrs
 {
-    private int mRadius = 15;
-    private int mDownSampling = 8;
-    private int mColor = Color.TRANSPARENT;
-    private boolean mAsync = false;
+    private int mRadius;
+    private int mDownSampling;
+    private int mColor;
+    private boolean mAsync;
 
-    public BlurViewAttrs(Context context, AttributeSet attrs)
+    private BlurViewAttrs(Context context, AttributeSet attrs)
     {
-        if (attrs == null)
-            return;
-
         final DefaultBlurSettings settings = DefaultBlurSettings.get(context);
         mRadius = settings.getRadius();
         mDownSampling = settings.getDownSampling();
         mColor = settings.getColor();
 
-        final TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.lib_blur_blur_view);
+        if (attrs != null)
+        {
+            final TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.lib_blur_blur_view);
 
-        mRadius = a.getInt(R.styleable.lib_blur_blur_view_blurRadius, mRadius);
-        mDownSampling = a.getInt(R.styleable.lib_blur_blur_view_blurDownSampling, mDownSampling);
-        mColor = a.getInt(R.styleable.lib_blur_blur_view_blurColor, mColor);
-        mAsync = a.getBoolean(R.styleable.lib_blur_blur_view_blurAsync, mAsync);
+            mRadius = a.getInt(R.styleable.lib_blur_blur_view_blurRadius, mRadius);
+            mDownSampling = a.getInt(R.styleable.lib_blur_blur_view_blurDownSampling, mDownSampling);
+            mColor = a.getInt(R.styleable.lib_blur_blur_view_blurColor, mColor);
+            mAsync = a.getBoolean(R.styleable.lib_blur_blur_view_blurAsync, mAsync);
 
-        a.recycle();
+            a.recycle();
+        }
+    }
+
+    public static BlurViewAttrs parse(Context context, AttributeSet attrs)
+    {
+        return new BlurViewAttrs(context, attrs);
     }
 
     public int getRadius()

c926ea52729f41a3f4cf565cbcdb56b0f6247f25
zjun <565061763@qq.com>
2018-07-10 18:57:21 +0800

    refactor

--- lib/src/main/java/com/fanwe/lib/blur/api/SimpleBlurApi.java
+++ lib/src/main/java/com/fanwe/lib/blur/api/SimpleBlurApi.java
@@ -10,7 +10,7 @@ import com.fanwe.lib.blur.core.BlurFactory;
 
 import java.lang.ref.WeakReference;
 import java.util.Map;
-import java.util.WeakHashMap;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -172,13 +172,21 @@ class SimpleBlurApi implements BlurApi, BlurApi.Config
                     {
                         synchronized (mBlur)
                         {
-                            target.onBlurred(blurSource());
+                            try
+                            {
+                                target.onBlurred(blurSource());
+                            } finally
+                            {
+                                mMapInvoker.remove(InternalInvoker.this);
+                                if (mBlur.isDestroyAfterBlur())
+                                    mBlur.destroy();
+                            }
                         }
                     }
                 });
 
                 if (mMapInvoker == null)
-                    mMapInvoker = new WeakHashMap<>();
+                    mMapInvoker = new ConcurrentHashMap<>();
                 mMapInvoker.put(this, future);
             } else
             {

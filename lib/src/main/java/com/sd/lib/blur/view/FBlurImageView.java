6efccefd07b972d33e16dd93ffb37b8180f732f3
zhengjun <565061763@qq.com>
2019-06-01 21:54:50 +0800

    FBlurImageViewProxy

--- lib/src/main/java/com/sd/lib/blur/view/FBlurImageView.java
+++ lib/src/main/java/com/sd/lib/blur/view/FBlurImageView.java
@@ -1,149 +1,66 @@
 package com.sd.lib.blur.view;
 
 import android.content.Context;
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 
-import com.sd.lib.blur.api.BlurApi;
-import com.sd.lib.blur.api.BlurApiFactory;
-
 public class FBlurImageView extends ImageView implements BlurView
 {
-    private BlurApi mBlurApi;
-    private boolean mBlurAsync;
-
-    private Drawable mOriginalDrawable;
-    private boolean mIsAttachedToWindow;
+    private final FBlurImageViewProxy mBlurImageViewProxy;
 
     public FBlurImageView(Context context, AttributeSet attrs)
     {
         super(context, attrs);
-
-        final BlurViewAttrs viewAttrs = BlurViewAttrs.parse(context, attrs);
-        setBlurRadius(viewAttrs.getBlurRadius());
-        setBlurDownSampling(viewAttrs.getBlurDownSampling());
-        setBlurColor(viewAttrs.getBlurColor());
-        setBlurAsync(viewAttrs.isBlurAsync());
-
-        if (getBackground() == null)
-            setBackgroundColor(viewAttrs.getBlurColor());
-    }
-
-    private BlurApi getBlurApi()
-    {
-        if (mBlurApi == null)
+        mBlurImageViewProxy = new FBlurImageViewProxy(this, attrs)
         {
-            mBlurApi = BlurApiFactory.create(getContext());
-            mBlurApi.setDestroyAfterBlur(false);
-        }
-        return mBlurApi;
+            @Override
+            protected void setImageDrawableSuper(Drawable drawable)
+            {
+                FBlurImageView.super.setImageDrawable(drawable);
+            }
+        };
     }
 
     @Override
     public final void setBlurRadius(int radius)
     {
-        getBlurApi().setRadius(radius);
+        mBlurImageViewProxy.setBlurRadius(radius);
     }
 
     @Override
     public final void setBlurDownSampling(int downSampling)
     {
-        getBlurApi().setDownSampling(downSampling);
+        mBlurImageViewProxy.setBlurDownSampling(downSampling);
     }
 
     @Override
     public final void setBlurColor(int color)
     {
-        getBlurApi().setColor(color);
+        mBlurImageViewProxy.setBlurColor(color);
     }
 
     @Override
     public final void setBlurAsync(boolean async)
     {
-        mBlurAsync = async;
+        mBlurImageViewProxy.setBlurAsync(async);
     }
 
     @Override
     public final void blur()
     {
-        setImageDrawable(mOriginalDrawable);
+        mBlurImageViewProxy.blur();
     }
 
     @Override
     public void setImageResource(int resId)
     {
-        final Drawable drawable = getResources().getDrawable(resId);
-        setImageDrawable(drawable);
+        mBlurImageViewProxy.setImageResourceOverride(resId);
     }
 
     @Override
     public void setImageDrawable(Drawable drawable)
     {
-        if (!(drawable instanceof BlurredBitmapDrawable))
-            mOriginalDrawable = drawable;
-
-        if (drawable == null || drawable instanceof BlurredBitmapDrawable)
-            super.setImageDrawable(drawable);
-        else
-            blurDrawable(drawable);
-    }
-
-    private void blurDrawable(Drawable drawable)
-    {
-        if (!mIsAttachedToWindow)
-            return;
-
-        if (drawable instanceof BlurredBitmapDrawable)
-            throw new IllegalArgumentException("can not blur BlurredBitmapDrawable");
-
-        if (mBlurAsync)
-        {
-            getBlurApi().blur(drawable).async().into(new BlurApi.Target()
-            {
-                @Override
-                public void onBlurred(Bitmap bitmap)
-                {
-                    applyBlur(bitmap);
-                }
-            });
-        } else
-        {
-            applyBlur(getBlurApi().blur(drawable).bitmap());
-        }
-    }
-
-    private void applyBlur(Bitmap bitmap)
-    {
-        if (bitmap != null)
-            setImageDrawable(new BlurredBitmapDrawable(getResources(), bitmap));
-    }
-
-    @Override
-    protected void onAttachedToWindow()
-    {
-        super.onAttachedToWindow();
-        mIsAttachedToWindow = true;
-        blur();
-    }
-
-    @Override
-    protected void onDetachedFromWindow()
-    {
-        super.onDetachedFromWindow();
-        mIsAttachedToWindow = false;
-        if (mBlurApi != null)
-            mBlurApi.destroy();
-    }
-
-    private static final class BlurredBitmapDrawable extends BitmapDrawable
-    {
-        private BlurredBitmapDrawable(Resources res, Bitmap bitmap)
-        {
-            super(res, bitmap);
-        }
+        mBlurImageViewProxy.setImageDrawableOverride(drawable);
     }
 }
---
+++ lib/src/main/java/com/sd/lib/blur/view/FBlurImageViewProxy.java
@@ -0,0 +1,164 @@
+package com.sd.lib.blur.view;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.sd.lib.blur.api.BlurApi;
+import com.sd.lib.blur.api.BlurApiFactory;
+
+public abstract class FBlurImageViewProxy implements BlurView
+{
+    private final Context mContext;
+
+    private BlurApi mBlurApi;
+    private boolean mBlurAsync;
+
+    private Drawable mOriginalDrawable;
+    private boolean mIsAttachedToWindow;
+
+    public FBlurImageViewProxy(ImageView imageView, AttributeSet attrs)
+    {
+        mContext = imageView.getContext();
+
+        final BlurViewAttrs viewAttrs = BlurViewAttrs.parse(mContext, attrs);
+        setBlurRadius(viewAttrs.getBlurRadius());
+        setBlurDownSampling(viewAttrs.getBlurDownSampling());
+        setBlurColor(viewAttrs.getBlurColor());
+        setBlurAsync(viewAttrs.isBlurAsync());
+
+        if (imageView.getBackground() == null)
+            imageView.setBackgroundColor(viewAttrs.getBlurColor());
+
+        mOriginalDrawable = imageView.getDrawable();
+        imageView.addOnAttachStateChangeListener(new View.OnAttachStateChangeListener()
+        {
+            @Override
+            public void onViewAttachedToWindow(View v)
+            {
+                mIsAttachedToWindow = true;
+                blur();
+            }
+
+            @Override
+            public void onViewDetachedFromWindow(View v)
+            {
+                mIsAttachedToWindow = false;
+                if (mBlurApi != null)
+                    mBlurApi.destroy();
+            }
+        });
+    }
+
+    private BlurApi getBlurApi()
+    {
+        if (mBlurApi == null)
+        {
+            mBlurApi = BlurApiFactory.create(mContext);
+            mBlurApi.setDestroyAfterBlur(false);
+        }
+        return mBlurApi;
+    }
+
+    @Override
+    public final void setBlurRadius(int radius)
+    {
+        getBlurApi().setRadius(radius);
+    }
+
+    @Override
+    public final void setBlurDownSampling(int downSampling)
+    {
+        getBlurApi().setDownSampling(downSampling);
+    }
+
+    @Override
+    public final void setBlurColor(int color)
+    {
+        getBlurApi().setColor(color);
+    }
+
+    @Override
+    public final void setBlurAsync(boolean async)
+    {
+        mBlurAsync = async;
+    }
+
+    @Override
+    public final void blur()
+    {
+        setImageDrawableOverride(mOriginalDrawable);
+    }
+
+    /**
+     * 覆盖ImageView的此方法
+     *
+     * @param resId
+     */
+    public void setImageResourceOverride(int resId)
+    {
+        final Drawable drawable = mContext.getResources().getDrawable(resId);
+        setImageDrawableOverride(drawable);
+    }
+
+    /**
+     * 覆盖ImageView的此方法
+     *
+     * @param drawable
+     */
+    public void setImageDrawableOverride(Drawable drawable)
+    {
+        if (!(drawable instanceof BlurredBitmapDrawable))
+            mOriginalDrawable = drawable;
+
+        if (drawable == null || drawable instanceof BlurredBitmapDrawable)
+            setImageDrawableSuper(drawable);
+        else
+            blurDrawable(drawable);
+    }
+
+    private void blurDrawable(Drawable drawable)
+    {
+        if (!mIsAttachedToWindow)
+            return;
+
+        if (drawable instanceof BlurredBitmapDrawable)
+            throw new IllegalArgumentException("can not blur BlurredBitmapDrawable");
+
+        if (mBlurAsync)
+        {
+            getBlurApi().blur(drawable).async().into(new BlurApi.Target()
+            {
+                @Override
+                public void onBlurred(Bitmap bitmap)
+                {
+                    applyBlur(bitmap);
+                }
+            });
+        } else
+        {
+            applyBlur(getBlurApi().blur(drawable).bitmap());
+        }
+    }
+
+    private void applyBlur(Bitmap bitmap)
+    {
+        if (bitmap != null)
+            setImageDrawableSuper(new BlurredBitmapDrawable(mContext.getResources(), bitmap));
+    }
+
+    protected abstract void setImageDrawableSuper(Drawable drawable);
+
+    private static final class BlurredBitmapDrawable extends BitmapDrawable
+    {
+        private BlurredBitmapDrawable(Resources res, Bitmap bitmap)
+        {
+            super(res, bitmap);
+        }
+    }
+}
